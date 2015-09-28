0248670341399fa300f1126c627fce5226796533
nnunn <natasha@nnunn.ca>
2015-09-28 10:35:13 -0600

    making changes to about and contact page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,13 +1,46 @@
 
 <div class="row">
 	<div class="span7">
-		<h2>About the Bruce Peel Special Collections &amp; Archives</h2>
-	<p>Bruce Braden Peel (1916–1998) was Chief Librarian at the University of Alberta from 1955 to 1982. The publication of his <em>A Bibliography of the Prairie Provinces</em> in 1956 was a landmark achievement that documents the early settlement and publishing history of western Canada, and his bibliography continues to grow today as an online database called "Peel's Prairie Provinces." Named to honour Peel's accomplishments, the Bruce Peel Special Collections Library houses a world-class collection of more than 100,000 rare books and a growing collection of archival materials in a climate-controlled and secure environment.  Thanks to visionary collectors and generous donors, a range of important collections treat numerous local and international subjects, from the Aboriginal peoples of North and South America to Historic Entomology and from Book History to the Chinese Experience in Canada. Researchers and visitors are welcome to browse the current <a href="/exhibits">exhibition</a> or examine materials in the Gregory Javitch Reading Room on weekday afternoons throughout the year. </p>
+				<h2>About Bruce Peel Special Collections Library</h2>
 
-	<p>The Peel library offers custom teaching programs for visiting <a href="classes">classes</a> from eighteen faculties and departments at U of A, as well as from other local universities, schools, and community groups.  The Library’s popular <a href="/exhibits">exhibitions</a> and award-winning catalogues allow visitors to view library treasures with guidance from expert curators on range of topics from the culture and history of the Canadian Prairies to modern artists’ books and from Virginia Woolf ’s Hogarth Press to that archetypal Mountie, <a href="http://steele.library.ualberta.ca/index.html">Sam Steele</a>.</p>
+		<p>Thanks to visionary collectors and generous donors, a range of important collections in 
+			Bruce Peel Special Collections &amp; Archives record the history of the University of Alberta 
+			and treat numerous local and international subjects, from the Aboriginal peoples of North and South 
+			America to Historic Entomology and from Book History to the Chinese Experience in Canada.</p> 
 
-	<p>The Peel library is a climate-controlled space: ideally maintaining a steady temperature (18-20°C) and relative humidity (45-55%) all year round. It is a secure, closed-stack library, which offers visitors access to library materials in a supervised reading room. As the stewards of this collection, staff members are committed to both preservation and conservation. “Preservation” refers to the actions taken to minimize the chemical and physical deterioration and damage to cultural property that can occur through theft, vandalism, displacement, shocks/vibrations, fire, water, light (visible and ultraviolet), temperature, humidity, pests, dust, and other pollutants. “Conservation” is the examination, documentation, treatment, and preventive care of cultural property on behalf of present and future generations.</p>
-<iframe src="https://www.360cities.net/embed_iframe/bruce-peel" class="hidden-phone" width="600" height="400" frameborder="0" bgcolor="#000000" target="_blank" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe><br/><a title="Panorama photos of Bruce Peel Special Collections on 360cities.net" href="http://www.360cities.net/image/bruce-peel" class="hidden-phone">Bruce Peel Special Collections</a>
+		<p>Bruce Braden Peel (1916–1998) was Chief Librarian at the University of Alberta from 1955 to 1982. 
+			The publication of his <em>A Bibliography of the Prairie Provinces</em> in 1956 was a landmark achievement that 
+			documents the early settlement and publishing history of western Canada, and his bibliography continues to 
+			grow today as an online database called "Peel's Prairie Provinces." Named to honour Peel's accomplishments, 
+			Bruce Peel Special Collections &amp; Archives houses a world-class collection of more than 100,000 rare books 
+			and a significant collection of archival materials in two locations. Located in the basement of the Rutherford 
+			South building on the main university campus, researchers and visitors are welcome to visit Special Collections 
+			(also called the "Peel library") to browse the current <a href="/exhibits">exhibition</a> or examine rare materials in the Gregory 
+			Javitch Reading Room on weekday afternoons throughout the year. Currently located off campus (on 50th Street), 
+			but expected to relocate in 2017 to the Research &amp; Collections Resource Facility (RCRF) to be built on South Campus, 
+			the University Archives Reading Room is open to researchers by appointment. Detailed 
+			information on visiting both of our locations can be found <a href="visit">here</a>.</p>
+
+		<p>The University Archives is the official repository for the permanently valuable records of the university and its affiliated 
+			institutions. Archival materials that are of continuing administrative, legal, or historical value include administrative 
+			and academic records, university publications, and select private papers of faculty, staff, students, alumni, and various 
+			university-related organizations. The Archives also holds the records of many notable individuals and organizations relating 
+			to local, provincial, and federal politics, the oil sands, education, and western settlement.</p>
+
+		<p>The Peel library offers custom teaching programs for visiting <a href="classes">classes</a> from eighteen faculties and departments at U of A, 
+			as well as from other local universities, schools, and community groups. The library’s popular exhibitions and award-winning 
+			catalogues allow visitors to view library treasures with guidance from expert curators on range of topics from the culture 
+			and history of the Canadian Prairies to modern artists’ books and from Virginia Woolf’s Hogarth Press to that archetypal Mountie, Sam Steele.</p>
+
+		<p>The Peel library is a climate-controlled space: ideally maintaining a steady temperature (18-20°C) and relative humidity (45-55%) all year 
+			round. It is a secure, closed-stack library, which offers visitors access to library materials in a supervised reading room. 
+			As the stewards of this collection, staff members are committed to both preservation and conservation. “Preservation” refers 
+			to the actions taken to minimize the chemical and physical deterioration and damage to cultural property that can occur through theft, 
+			vandalism, displacement, shocks/vibrations, fire, water, light (visible and ultraviolet), temperature, humidity, pests, dust, and other 
+			pollutants. “Conservation” is the examination, documentation, treatment, and preventive care of cultural property on behalf of 
+			present and future generations.</p>
+
+			<iframe src="https://www.360cities.net/embed_iframe/bruce-peel" class="hidden-phone" width="600" height="400" frameborder="0" bgcolor="#000000" target="_blank" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe><br/><a title="Panorama photos of Bruce Peel Special Collections on 360cities.net" href="http://www.360cities.net/image/bruce-peel" class="hidden-phone">Bruce Peel Special Collections</a>
 </div>
 <div class="span5 getting">
 	<h3>FAQs</h3>
@@ -15,7 +48,7 @@
  
 <p>Please consult this <a href="http://www.rbms.info/yob.shtml">online guide</a> sponsored by the Rare Books and Manuscripts Section of the Association of College and Research Libraries, the Antiquarian Booksellers’ Association of America, and the Rare Book School at the University of Virginia.</p>
 
- <p class="question">What is the difference between the Bruce Peel Special Collections Library and Peel's Prairie Provinces database? </p>
+ <p class="question">What is the difference between the Peel library and Peel's Prairie Provinces database? </p>
 
 <p>Researchers often mistakenly imagine that the library and the database are one and the same because 
 	both are named to honour Bruce Peel. <a href="http://peel.library.ualberta.ca/index.html">Peel’s 
--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -1,90 +1,143 @@
-<div class="row">
-<div class="span9">
-<h2>Contact</h2>
+<h2> WE HAVE TWO LOCATIONS!</h2>
 <div class="row">
 	<div class="span9">
-	<img src="/assets/img/interior.jpg" class="contact-image hidden-phone"/>
-	<strong> Mailing address:</strong><br/>
-Bruce Peel Special Collections &amp; Archives<br/>
-University of Alberta<br/>
-B7 Rutherford South<br/>
-Edmonton, AB  T6G 2J4<br/><br/>
+		<h2>Bruce Peel Special Collections:</h2>
+			<div class="row">
+				<div class="span9">
+					<img src="/assets/img/interior.jpg" class="contact-image hidden-phone"/>
+					<strong> Mailing address:</strong><br/>
+					Bruce Peel Special Collections &amp; Archives<br/>
+					University of Alberta<br/>
+					B7 Rutherford South<br/>
+					Edmonton, AB  T6G 2J4<br/><br/>
 
-<strong> General email:</strong><br/>
-<a href="mailto:bpsc@ualberta.ca">bpsc@ualberta.ca</a><br/><br/>
+					<strong> General email:</strong><br/>
+					<a href="mailto:bpsc@ualberta.ca">bpsc@ualberta.ca</a><br/><br/>
 
-<strong> General phone:</strong><br/>
-780-492-5998 (Mon-Fri 12:00-4:30pm)<br/>
-<br/>
-</div>
-</div>
+					<strong> General phone:</strong><br/>
+					780-492-5998 (Mon-Fri 12:00-4:30pm)<br/>
+					<br/>
+				</div>
+			</div>
+			<div class="row">
+				<h3 class="span12">Staff Directory</h3>
+				<div class="span3">
+
+					<strong>Robert Desmarais</strong><br />
+					Head Special Collections Librarian<br />
+					<a href="mailto:robert.desmarais@ualberta.ca">robert.desmarais@ualberta.ca</a><br />
+					780-492-7929<br/>
+
+					<br />
+
+					<strong>Linda Quirk</strong><br />
+					Assistant Special Collections Librarian<br />
+					<a href="mailto:linda.quirk@ualberta.ca">linda.quirk@ualberta.ca</a><br />
+					780-492-7928<br />
+
+					<br />
+
+					
+				</div>
+				<div class="span3">
+
+
+					<strong>Jeff Papineau</strong><br />
+					Library Assistant<br />
+					<a href="mailto:jeff.papineau@ualberta.ca">jeff.papineau@ualberta.ca</a><br />
+					780-492-7311<br />
+					<br />
+
+
+					<strong>Carol Irwin</strong><br />
+					Library Assistant<br />
+					<a href="mailto:carol.irwin@ualberta.ca">carol.irwin@ualberta.ca</a><br />
+					780-492-7311<br />
+					<br />
+				</div>
+				<div class="span3">
+
+					 <strong>Carolyn Morgan </strong><br />
+					Conservator <br />
+					<a href="mailto:carolyn.morgan@ualberta.ca ">carolyn.morgan@ualberta.ca </a><br />
+					780-492-1902<br />
+					<br />
+
+					<strong>Brian Stearns</strong><br />
+					Cataloguing Librarian<br />
+					<a href="mailto:brian.stearns@ualberta.ca">brian.stearns@ualberta.ca</a><br />
+					780-492-7603<br /><br />
+
+				</div>
+			</div>
+			<p>If you are unsure who to contact, you can write to any one of us and we will forward your message to the appropriate person.</p>
+		</div>
+		<div class="span3 getting credits">
+			<h3>Credits</h3>
+			<p>Thank you to the following people for their contributions to the Peel library website:<p>
+			<ul class="bottom-margin">
+				<li class="first"><strong>Photography and Scanning:</strong> Jeff Papineau</li>
+				<li><strong>Editing:</strong> Robert Desmarais and Cheryl Cundell</li>
+				<li><strong>Content:</strong> Linda Quirk, Carolyn Morgan, Kymberly Sobchyshyn, Colette Leung, Michele Pagliuso, Denis Lacroix, and Lynn McPherson</li>
+				<li><strong>Web Programming and Design:</strong> Natasha Nunn</li>
+			</ul>
+		</div>
+	</div>
 <div class="row">
-	<h3 class="span12">Staff Directory</h3>
-	<div class="span3">
+	<div class="span12">
+		<h2>University Archives:</h2>
+			<div class="row">
+				<div class="span9">
+					<img src="/assets/img/archives-interior.jpg" class="contact-image hidden-phone"/>
+					<strong> Mailing address:</strong><br/>
+					Book and Record Depository (BARD)</br/>
+					100, 8170-50 Street<br/>
+					Edmonton, Alberta<br/>
+					Canada, T6B 1E6<br/><br/>
 
-<strong>Robert Desmarais</strong><br />
-Head Special Collections Librarian<br />
-<a href="mailto:robert.desmarais@ualberta.ca">robert.desmarais@ualberta.ca</a><br />
-780-492-7929<br/>
+					<strong> General email:</strong><br/>
+					<a href="mailto:archives@ualberta.ca">archives@ualberta.ca</a><br/><br/>
 
-<br />
+					<strong> General phone:</strong><br/>
+					780-248-1300<br/>
+					<br/>
+				</div>
+			</div>
+			<div class="row">
+				<h3 class="span12">Staff Directory</h3>
+				<div class="span3">
 
-<strong>Linda Quirk</strong><br />
-Assistant Special Collections Librarian<br />
-<a href="mailto:linda.quirk@ualberta.ca">linda.quirk@ualberta.ca</a><br />
-780-492-7928<br />
+					<strong>James Franks</strong><br />
+					University Records Archivist<br />
+					<a href="mailto:james.franks@ualberta.ca">james.franks@ualberta.ca</a><br />
+					780-248-1304<br/>
 
-<br />
-
-<strong>Brian Stearns</strong><br />
-Cataloguing Librarian<br />
-<a href="mailto:brian.stearns@ualberta.ca">brian.stearns@ualberta.ca</a><br />
-780-492-7603<br /><br />
-</div>
-<div class="span3">
-
-
-<strong>Jeff Papineau</strong><br />
-Library Assistant<br />
-<a href="mailto:jeff.papineau@ualberta.ca">jeff.papineau@ualberta.ca</a><br />
-780-492-7311<br />
-<br />
+					<br />
+					
+				</div>
+				<div class="span3">
+					<strong>Alicia Odeen</strong><br />
+					Archives Assistant<br />
+					<a href="mailto:alicia.odeen@ualberta.ca ">odeen@ualberta.ca</a><br />
+					780-248-1302<br />
+					<br />
 
 
-<strong>Carol Irwin</strong><br />
-Library Assistant<br />
-<a href="mailto:carol.irwin@ualberta.ca">carol.irwin@ualberta.ca</a><br />
-780-492-7311<br />
-<br />
-</div>
-<div class="span3">
+				</div>
+				<div class="span3">
 
- <strong>Carolyn Morgan </strong><br />
-Conservator <br />
-<a href="mailto:carolyn.morgan@ualberta.ca ">carolyn.morgan@ualberta.ca </a><br />
-780-492-1902<br />
-<br />
+						<strong>Krista Jamieson</strong><br />
+						Sessional Archivist<br />
+					Human Rights Institute of Canada Project<br />
+					<a href="mailto:kjamieso@ualberta.ca">kjamieso@ualberta.ca</a><br />
+					780-248-1771<br />
 
-<strong>Alicia Odeen</strong><br />
-Library Assistant<br />
-<a href="mailto:alicia.odeen@ualberta.ca ">odeen@ualberta.ca </a><br />
-780-248-1302<br />
-<br />
-</div>
-</div>
-<p>If you are unsure who to contact, you can write to any one of us and we will forward your message to the appropriate person.</p>
-</div>
-<div class="span3 getting credits">
+					<br />	
 
-<h3>Credits</h3>
-		<p>Thank you to the following people for their contributions to the Peel library website:<p>
-<ul class="bottom-margin">
-<li class="first"><strong>Photography and Scanning:</strong> Jeff Papineau</li>
-<li><strong>Editing:</strong> Robert Desmarais and Cheryl Cundell</li>
-<li><strong>Content:</strong> Linda Quirk, Carolyn Morgan, Kymberly Sobchyshyn, Colette Leung, Michele Pagliuso, Denis Lacroix, and Lynn McPherson</li>
-<li><strong>Web Programming and Design:</strong> Natasha Nunn</li>
-</ul>
-
-</div>
-</div>
+				</div>
+			</div>
+			<p>If you are unsure who to contact, you can write to any one of us and we will forward your message to the appropriate person.</p>
+		</div>
+					
+	</div>
 </div>
