99f9b85b7889e6fc76f1e7faf384167b7c085165
Natasha Nunn <natasha@nnunn.ca>
2020-09-21 13:51:24 -0600

    Update footer.php

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -11,12 +11,12 @@
           <div class="span7">
       	<?php if (uri_string() == ""):?>
       	<p class="hidden-phone"><a href="info/slide-info" class="slide-info">Source information for background images</a></p>
-            <p><a href="https://www.library.ualberta.ca/about-us/partners/#collaborate" class="slide-info">Open to Collaborate</a></p>
+           
       	<?php endif ?>
       </div>
         <p class="span5">Copyright 2015 &copy; <a href="http://www.library.ualberta.ca">University of Alberta Libraries</a>, 
           Bruce Peel Special Collections, B7 Rutherford South, Edmonton, AB, Canada T6G 2J4 -  
-          <a href="http://www.library.ualberta.ca/privacy/">Web Site Privacy Policy</a></p>
+          <a href="http://www.library.ualberta.ca/privacy/">Web Site Privacy Policy</a>  :: <a href="https://www.library.ualberta.ca/about-us/partners/#collaborate">Open to Collaborate</a></p>
       </div>
     </div>
     </div>
