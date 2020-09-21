459b35ea9e861d78ceda532c9ba2378ced86d3d7
Natasha Nunn <natasha@nnunn.ca>
2020-09-21 10:24:57 -0600

    Update footer.php

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -11,6 +11,7 @@
           <div class="span7">
       	<?php if (uri_string() == ""):?>
       	<p class="hidden-phone"><a href="info/slide-info" class="slide-info">Source information for background images</a></p>
+            <p><a href="https://www.library.ualberta.ca/about-us/partners/#collaborate" class="slide-info">Open to Collaborate</a></p>
       	<?php endif ?>
       </div>
         <p class="span5">Copyright 2015 &copy; <a href="http://www.library.ualberta.ca">University of Alberta Libraries</a>, 
