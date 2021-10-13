4895eb29257a0302f9fe3bea08bd5c0c2f13ab17
Natasha Nunn <natasha@nnunn.ca>
2021-10-13 14:25:57 -0600

    Update header.php

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -4,7 +4,7 @@
     <title>BPSC Library</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+     <link rel="shortcut icon" type="image/x-icon" href="https://www.ualberta.ca/favicon.svg" />
 
     <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
     <!-- Bootstrap -->
