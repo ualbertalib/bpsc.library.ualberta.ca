92f617b16ccaa8e358cea5d455c1ce2ab6445823
Natasha Nunn <natasha@nnunn.ca>
2023-04-05 14:43:53 -0600

    Update workshops.php

--- application/views/pages/workshops.php
+++ application/views/pages/workshops.php
@@ -3,7 +3,7 @@
 <div class="span7">
 	<img src="/assets/img/workshop.jpg" class="hidden-phone" style="margin: 20px;" />
 <h1>Bruce Peel Special Collections Workshops</h1>
-<p class="top-margin">Bruce Peel Special Collections offers a series of online and in-person workshops each term to provide a wide range of learning opportunities to those University of Alberta students who are motivated to sign up. We receive far more requests for class visits than we can accommodate, so offer Peel Workshops as an alternative to such class visits. These workshops explore generalized versions of a selection of the topics that we have been teaching for years. The schedule/topics are posted well before the beginning of each term and registration opens on the first day of classes.</p>
+<p class="top-margin">Bruce Peel Special Collections offers a series of online and in-person workshops each term to provide a wide range of learning opportunities to those University of Alberta students and employees who are motivated to sign up. These workshops explore a selection of topics that we have been teaching for years. The schedule/topics are posted well before the beginning of each term and registration opens on the first day of classes.</p>
 <p><a href="https://docs.google.com/document/d/1-p53TKnhBIcE27NRdf-WTmuzAGamVGzy6DpjWi5Xxt8" class="myButton">Peel Workshop Schedule</a></p>
 <p><a href="http://ualberta.libcal.com/calendar/workshops/?cid=7360&t=d&d=0000-00-00&cal=7360" class="myButton">BPSC Workshop Registration &amp; Calendar</a></p>
 
