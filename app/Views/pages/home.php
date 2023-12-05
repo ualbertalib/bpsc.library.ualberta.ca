0db40750efa1d8a27d41ee5a916a77d079f65baa
jhennig <jeremy.hennig@ualberta.ca>
2023-12-05 15:44:13 -0700

    elevator update

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,19 +1,23 @@
 
     <div class="row-fluid slide-area hidden-phone">
     </div>
-    
+
     <div class="row-fluid main-content">
       <div class="container">
-      <div class="span7 front">
-        
+      <div class="span7 front" style='height: 400px'><!-- Height overide  custom.css -->
+
                  <p class="welcome">Thanks to visionary collectors and generous donors, Bruce Peel Special Collections houses a world-class collection of more than 100,000 rare books and a significant collection of archival materials that explore a range of local and international subjects. The Peel library is open most weekday afternoons throughout the year for students, faculty, staff, and visitors to browse the current exhibition or examine rare materials in the <a href="https://bpsc.library.ualberta.ca/info/visit">reading rooms</a>.</p>
-                   <p class="welcome">TO VIEW MATERIALS HELD IN BRUCE PEEL SPECIAL COLLECTIONS, please write to us at <a href="mailto:bpsc@ualberta.ca">bpsc@ualberta.ca</a> to request an appointment well 
+                   <p class="welcome">TO VIEW MATERIALS HELD IN BRUCE PEEL SPECIAL COLLECTIONS, please write to us at <a href="mailto:bpsc@ualberta.ca">bpsc@ualberta.ca</a> to request an appointment well
 			   in advance, listing the requested materials including author, title, and call number for each item, and indicating which weekday afternoons would work (appointments are generally available Tuesday-Friday 1-4pm).</p>
-                
+
+				<p class="welcome"><strong>Please note: Peel can be accessed using stairs, but the only elevator that services Peel's basement location is expected to be out of service until early in 2024. We apologize for any inconvenience.</strong></p>
+
+
+
                 </div>
-          
+
     <div class="span2 front hidden-phone">
-     
+
           <?php if (!empty($on_now)): ?>
             <?php foreach ($on_now as $on_now_item): ?>
               <?php if ($on_now_item['on_now'] != '0'): ?>
@@ -23,15 +27,15 @@
                   <a href="<?php echo $on_now_item['external_url'] ?>">
                 <?php else: ?>
                   <a href="exhibits/<?php echo $on_now_item['slug'] ?>">
-                <?php endif?>  
-            
+                <?php endif?>
+
                   <div class="on-now hidden-phone"><img src="/assets/uploads/display/<?php echo $on_now_item['slug']; ?>.jpg"/>
-                   
+
                   </div>
-                 
+
                 </a>
-                
-              <?php endif?>  
+
+              <?php endif?>
             <?php endforeach?>
           <?php else: ?>
             <a href="/exhibits">
@@ -43,31 +47,29 @@
                 </div>
               </div>
             </a>
-            <h2 class="dates">No Current Exhibit</h2>      
-          <?php endif?>  
+            <h2 class="dates">No Current Exhibit</h2>
+          <?php endif?>
           </div>
             <div class="span3 front">
-         
 
-       
+
+
            <h2>News</h2>
-		   
+
            <ul>
-				<?php 
+				<?php
 
           foreach($rss_news as $row){
 					echo "<li><a href='{$row['link']}'>" . $row['title'] . "</a></li>";
-					
+
 				} ?>
-             
-             
+
+
                </ul>
 
           </div>
       </div>
     </div>
- <div>   
- <div> 
+ <div>
+ <div>
   </div>
-
-
