5d913b62079a1fdc499398ca655869b607cd4595
janebanks <122636594+janebanks@users.noreply.github.com>
2024-02-08 10:56:40 -0700

    Update workshops.php
    Wording changes described in "Ticket#20240205104 — Updates to Bruce Peel Special Collections website"

--- app/Views/pages/workshops.php
+++ app/Views/pages/workshops.php
@@ -2,7 +2,7 @@
 <div class="span7">
 	<img src="/assets/img/workshop.jpg" class="hidden-phone" style="margin: 20px;" />
 <h1>Bruce Peel Special Collections Workshops</h1>
-<p class="top-margin">Bruce Peel Special Collections offers a series of online and in-person workshops each term to provide a wide range of learning opportunities to those University of Alberta students and employees who are motivated to sign up. These workshops explore a selection of topics that we have been teaching for years. The schedule/topics are posted well before the beginning of each term and registration opens on the first day of classes.</p>
+<p class="top-margin">Bruce Peel Special Collections offers a series of online and in-person workshops each term to provide a wide range of learning opportunities to those University of Alberta students and employees who are motivated to sign up. These workshops explore a selection of topics that we have been teaching for years. The schedule/topics are posted well before the beginning of each term to accommodate requests from those professors who sometimes use Peel Workshops in their teaching/assignments. Registration opens on the first day of classes.</p>
 <p><a href="https://docs.google.com/document/d/1-p53TKnhBIcE27NRdf-WTmuzAGamVGzy6DpjWi5Xxt8" class="myButton">Peel Workshop Schedule</a></p>
 <p><a href="http://ualberta.libcal.com/calendar/workshops/?cid=7360&t=d&d=0000-00-00&cal=7360" class="myButton">BPSC Workshop Registration &amp; Calendar</a></p>
 </div>
